d46791173c96ef5d9c00dee1045861a51ce139d4
Carlos Sierra <carlos.sierra@liferay.com>
2019-06-07 15:36:36 +0200

    Do not use service trackers and one servlet per service

--- modules/apps/portal-vulcan/portal-vulcan-impl/src/main/java/com/liferay/portal/vulcan/internal/graphql/servlet/GraphQLServletExtender.java
+++ modules/apps/portal-vulcan/portal-vulcan-impl/src/main/java/com/liferay/portal/vulcan/internal/graphql/servlet/GraphQLServletExtender.java
@@ -50,15 +50,12 @@ import javax.servlet.Servlet;
 import javax.servlet.http.HttpServletRequest;
 
 import org.osgi.framework.BundleContext;
-import org.osgi.framework.ServiceReference;
 import org.osgi.framework.ServiceRegistration;
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Deactivate;
 import org.osgi.service.http.context.ServletContextHelper;
 import org.osgi.service.http.whiteboard.HttpWhiteboardConstants;
-import org.osgi.util.tracker.ServiceTracker;
-import org.osgi.util.tracker.ServiceTrackerCustomizer;
 
 /**
  * @author Preston Crary
@@ -137,147 +134,14 @@ public class GraphQLServletExtender {
 			}
 		};
 
-		_serviceTracker = new ServiceTracker<>(
-			bundleContext, ServletData.class,
-			new ServletDataServiceTrackerCustomizer(bundleContext));
 
-		_serviceTracker.open();
 	}
 
 	@Deactivate
 	protected void deactivate() {
-		_serviceTracker.close();
 	}
 
 	private DefaultTypeFunction _defaultTypeFunction;
 	private GraphQLObjectHandler _graphQLObjectHandler;
-	private ServiceTracker<?, ?> _serviceTracker;
-
-	private class ServletDataServiceTrackerCustomizer
-		implements ServiceTrackerCustomizer
-			<ServletData, Collection<ServiceRegistration<?>>> {
-
-		@Override
-		public Collection<ServiceRegistration<?>> addingService(
-			ServiceReference<ServletData> serviceReference) {
-
-			// Schema
-
-			GraphQLSchema.Builder schemaBuilder = GraphQLSchema.newSchema();
-
-			ServletData servletData = _bundleContext.getService(
-				serviceReference);
-
-			Object mutation = servletData.getMutation();
-
-			ProcessingElementsContainer processingElementsContainer =
-				new ProcessingElementsContainer(_defaultTypeFunction);
-
-			schemaBuilder.mutation(
-				_graphQLObjectHandler.getObject(
-					mutation.getClass(), processingElementsContainer));
-
-			Object query = servletData.getQuery();
-
-			schemaBuilder.query(
-				_graphQLObjectHandler.getObject(
-					query.getClass(), processingElementsContainer));
-
-			Dictionary<String, Object> properties = new HashMapDictionary<>();
-
-			Class<? extends ServletData> clazz = servletData.getClass();
-
-			servletData.setAcceptLanguageFunction(
-				object -> {
-					GraphQLContext graphQLContext = (GraphQLContext)object;
-
-					Optional<HttpServletRequest> httpServletRequestOptional =
-						graphQLContext.getHttpServletRequest();
-
-					return new AcceptLanguageImpl(
-						httpServletRequestOptional.orElse(null),
-						LanguageUtil.getLanguage(), PortalUtil.getPortal());
-				});
-
-			String path = servletData.getPath();
-
-			String servletContextName = path.split("/")[1];
-
-			properties.put(
-				HttpWhiteboardConstants.HTTP_WHITEBOARD_SERVLET_NAME,
-				clazz.getName());
-
-			properties.put(
-				HttpWhiteboardConstants.HTTP_WHITEBOARD_SERVLET_PATTERN, "/*");
-
-			properties.put(
-				HttpWhiteboardConstants.HTTP_WHITEBOARD_CONTEXT_SELECT,
-				servletContextName);
-
-			Dictionary<String, Object> helperProperties =
-				new HashMapDictionary<>();
-
-			helperProperties.put(
-				HttpWhiteboardConstants.HTTP_WHITEBOARD_CONTEXT_NAME,
-				servletContextName);
-			helperProperties.put(
-				HttpWhiteboardConstants.HTTP_WHITEBOARD_CONTEXT_PATH, path);
-			helperProperties.put(
-				HttpWhiteboardConstants.HTTP_WHITEBOARD_FILTER_SERVLET,
-				clazz.getName());
-
-			Collection<ServiceRegistration<?>> serviceRegistrations =
-				new ArrayList<>();
-
-			serviceRegistrations.add(
-				_bundleContext.registerService(
-					ServletContextHelper.class,
-					new ServletContextHelper(_bundleContext.getBundle()) {
-					},
-					helperProperties));
-
-			// Servlet
-
-			SimpleGraphQLHttpServlet.Builder servletBuilder =
-				SimpleGraphQLHttpServlet.newBuilder(schemaBuilder.build());
-
-			Servlet servlet = servletBuilder.build();
-
-			serviceRegistrations.add(
-				_bundleContext.registerService(
-					Servlet.class, servlet, properties));
-
-			return serviceRegistrations;
-		}
-
-		@Override
-		public void modifiedService(
-			ServiceReference<ServletData> serviceReference,
-			Collection<ServiceRegistration<?>> serviceRegistrations) {
-		}
-
-		@Override
-		public void removedService(
-			ServiceReference<ServletData> serviceReference,
-			Collection<ServiceRegistration<?>> serviceRegistrations) {
-
-			for (ServiceRegistration<?> serviceRegistration :
-					serviceRegistrations) {
-
-				serviceRegistration.unregister();
-			}
-
-			_bundleContext.ungetService(serviceReference);
-		}
-
-		private ServletDataServiceTrackerCustomizer(
-			BundleContext bundleContext) {
-
-			_bundleContext = bundleContext;
-		}
-
-		private final BundleContext _bundleContext;
-
-	}
 
 }
