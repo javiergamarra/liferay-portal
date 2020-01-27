05bff195b3a88caf7f97e8d67c9fbfe9aabddeea
Javier de Arcos <javier.dearcos@liferay.com>
2020-01-27 11:11:57 +0100

    LPS-107114 Add entity Id as filterable property

--- modules/apps/headless/headless-delivery/headless-delivery-impl/src/main/java/com/liferay/headless/delivery/internal/odata/entity/v1_0/BlogPostingEntityModel.java
+++ modules/apps/headless/headless-delivery/headless-delivery-impl/src/main/java/com/liferay/headless/delivery/internal/odata/entity/v1_0/BlogPostingEntityModel.java
@@ -20,6 +20,7 @@ import com.liferay.portal.odata.entity.ComplexEntityField;
 import com.liferay.portal.odata.entity.DateTimeEntityField;
 import com.liferay.portal.odata.entity.EntityField;
 import com.liferay.portal.odata.entity.EntityModel;
+import com.liferay.portal.odata.entity.IdEntityField;
 import com.liferay.portal.odata.entity.IntegerEntityField;
 import com.liferay.portal.odata.entity.StringEntityField;
 
@@ -33,6 +34,10 @@ public class BlogPostingEntityModel implements EntityModel {
 
 	public BlogPostingEntityModel(List<EntityField> entityFields) {
 		_entityFieldsMap = EntityModel.toEntityFieldsMap(
+			new IdEntityField(
+				"id",
+				locale -> Field.ENTRY_CLASS_PK,
+				String::valueOf),
 			new CollectionEntityField(
 				new IntegerEntityField(
 					"taxonomyCategoryIds", locale -> "assetCategoryIds")),
